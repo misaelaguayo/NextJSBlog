ae5fa3cec87b35ef13f0bc982fb9ee4976afc3be
MIsael Aguayo <aguayo.misael@gmail.com>
2021-11-07 09:11:20 -0600

    Removed sample code. Added husky pre-commit prettier check.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -9,12 +9,19 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I am a software engineer interested in information security. Some more of my interests include low level networking, reverse engineering, and virtual machine languages</p>
         <p>
-          This blog will be used to document some of the CTFs I participate and some of my interests.{' '}
-          <a href="https://www.linkedin.com/in/misael-aguayo-58b22985">LinkedIn</a>
+          I am a software engineer interested in information security. Some more
+          of my interests include low level networking, reverse engineering, and
+          virtual machine languages
+        </p>
+        <p>
+          This blog will be used to document some of the CTFs I participate and
+          some of my interests.{" "}
+          <a href="https://www.linkedin.com/in/misael-aguayo-58b22985">
+            LinkedIn
+          </a>
         </p>
       </section>
     </Layout>
-  )
+  );
 }
