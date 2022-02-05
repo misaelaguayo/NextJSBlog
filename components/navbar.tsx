0953c90e373d08e639615faa2549ed121bd40d0e
Misael Aguayo <aguayo.misael@gmail.com>
2022-02-05 17:15:13 -0600

    Added small improvements to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,10 +3,23 @@ import styles from "./navbar.module.css";
 const Navbar = () => {
   return (
     <div className={styles.nav}>
-      <ul>RESUME</ul>
-      <ul>WRITEUPS</ul>
-      <ul>GITHUB</ul>
-      <ul>TRYHACKME</ul>
+      <ul>
+        <a href="/Resume.docx" download>
+          RESUME
+        </a>
+      </ul>
+      <ul>
+        <a href="/posts/first-post">WRITEUPS</a>
+      </ul>
+      <ul>
+        <a href="https://github.com/misaelaguayo">GITHUB</a>
+      </ul>
+      <ul>
+        <a href="https://tryhackme.com/p/irishyogashirt">TRYHACKME</a>
+      </ul>
+      <ul>
+        <a href="/posts/certifications">CERTIFICATIONS</a>
+      </ul>
     </div>
   );
 };
--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,15 +1,20 @@
 import Head from "next/head";
 import Link from "next/link";
+import { CSSProperties } from "react";
 import Layout from "../../components/layout";
+
+const centerStyle: CSSProperties = {
+  textAlign: "center",
+};
 
 export default function FirstPost() {
   return (
     <Layout home={undefined}>
       <Head>
-        <title>First Post</title>
+        <title>Placeholder for now</title>
       </Head>
-      <h1>First Post</h1>
-      <h2>
+      <h1 style={centerStyle}>First Post</h1>
+      <h2 style={centerStyle}>
         <Link href="/">
           <a>Back to home</a>
         </Link>
