7ce31df166d1e084637afcda733d79d86f5d40ac
misael aguayo <aguayo.misael@gmail.com>
2022-10-16 09:15:43 -0500

    fixing async within useeffect

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -6,10 +6,13 @@ import { useEffect, useState } from "react";
 
 export default function FirstPost() {
   const [markdown, setMarkdown] = useState(null);
-  useEffect(async () => {
-    const response = await fetch("/markdown/hackpark.md");
-    const text = await response.text();
-    setMarkdown(text);
+  useEffect(() => {
+    async function markdown() {
+      const response = await fetch("/markdown/hackpark.md");
+      const text = await response.text();
+      setMarkdown(text);
+    }
+    markdown();
   }, []);
 
   return (
