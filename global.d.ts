ff19a291d24e65e8a66bad7759833e0122e7527a
misael aguayo <aguayo.misael@gmail.com>
2022-12-10 16:57:33 -0600

    semi working version

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,4 @@
 interface Window {
   loadPyodide: ({ indexURL: str }) => Promise<any>;
+  loadPackage: (str) => Promise<any>;
 }
--- pages/repl/index.tsx
+++ pages/repl/index.tsx
@@ -12,7 +12,11 @@ export function Repl() {
   function handleCommand(command: string) {
     if (pyodideInstance) {
       try {
-        setOutput(pyodideInstance.runPython(command));
+        setOutput(
+          pyodideInstance.runPython(
+            `from brd_package_misaelaguayo import Brd; Brd.run('${command}')`
+          )
+        );
       } catch (e) {
         console.log(e);
         setOutput("Some kinda python error");
@@ -25,6 +29,9 @@ export function Repl() {
         let pyodide = await window.loadPyodide({
           indexURL: "https://cdn.jsdelivr.net/pyodide/v0.20.0/full/",
         });
+        await pyodide.loadPackage(
+          "https://test-files.pythonhosted.org/packages/7a/22/a138c87326344bf240275943f9c681003549aeec42e4c3efcd72aea25a11/brd_package_misaelaguayo-0.0.3-py3-none-any.whl"
+        );
         setPyodideInstance(pyodide);
       }
     }
@@ -34,17 +41,16 @@ export function Repl() {
     <section className={replStyles.repl}>
       <h2>REPL</h2>
       <form>
-        <label>
-          {">"}
-          <input
-            type="text"
-            name="name"
-            value={command}
-            onChange={(event) => {
-              setCommand(event.target.value);
-            }}
-          />
-        </label>
+        <textarea
+          rows={4}
+          cols={50}
+          name="name"
+          value={command}
+          onChange={(event) => {
+            setCommand(event.target.value);
+          }}
+        />
+        <br />
         <input
           type="button"
           onClick={() => {
